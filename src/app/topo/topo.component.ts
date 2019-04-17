048c575677ad14c406002307080122f928050575
Lukas <lukas@hotmail.co.jp>
2019-04-16 22:57:02 -0300

    Property bind e string interpolation, mock e class do mockk

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
+  public titulo = 'Aprendendo Inglês';
+
   constructor() { }
 
   ngOnInit() {
